96af8135d58008ae080b297ed1b257b0e057f9cd
thandosim <sim24001@byui.edu>
2025-05-24 17:18:53 +0200

    deleted unused code. it was a reduant errorhandling function

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,15 +5,6 @@ const errorController = {
         next(new Error("Intentional 500 Error – This is a test!"));
     },
 
-    // handleErrors: (err, req, res, next) => {
-    //     console.error(`Error at: "${req.originalUrl}": ${err.message}`);
-        
-    //     res.status(err.status || 500).render("errors/error", {
-    //         title: err.status || "Server Error",
-    //         message: err.message || "Oops! Something went wrong.",
-    //         nav: req.nav || [] // Ensure nav is passed if needed
-    //     });
-    // }
 };
 
 module.exports = errorController;
