4cb1d0ccbaedeb7b810ddbd63a04f1a3aa900b90
thandosim <sim24001@byui.edu>
2025-05-24 14:53:57 +0200

    implemented error handling in inver=tory routes

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -2,10 +2,11 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 // Route to build detailed vehicle view
-router.get("/detail/:invId", invController.buildByInventoryId)
+router.get("/detail/:invId", utilities.handleErrors(invController.buildByInventoryId));
 
 module.exports = router;
