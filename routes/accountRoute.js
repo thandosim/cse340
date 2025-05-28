9c272499bdf2a53019a084e80119599901040585
thandosim <sim24001@byui.edu>
2025-05-28 13:05:45 +0200

    implemented serverside validatopn

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,12 +3,16 @@ const express = require("express")
 const router = new express.Router() 
 const accController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
 
 // Route to build account/login view
 router.get("/login", utilities.handleErrors(accController.buildLogin));
 // Route to build account/register view
 router.get("/register", utilities.handleErrors(accController.buildRegister));
 // Route to Post fome data into database
-router.post('/register', utilities.handleErrors(accController.registerAccount))
+router.post('/register', 
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accController.registerAccount))
 
 module.exports = router;
