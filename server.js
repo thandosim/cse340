259318f8d6826d5fffc22ab3b96504e091910ef5
thandosim <sim24001@byui.edu>
2025-06-04 14:23:13 +0200

    added cookie parser to server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const session = require("express-session")
 const pool = require('./database/')
 const accountRoute = require('./routes/accountRoute')
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
 /* ***********************
  * Middleware
@@ -41,6 +42,7 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
+app.use(cookieParser())
 
 /* ***********************
  * View Engine and Templates 
